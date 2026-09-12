8966eded6ff3e8d1fe80e97e9c3d5d15cfc056ea
migueldlr844-ui <migueldlr844@gmail.com>
2026-09-12 16:33:32 +0100

    Delete App directory

--- App/app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
-      <h1>Bienvenue sur DocExpress</h1>
-      <p>L'application est en cours d'initialisation.</p>
-    </main>
-  );
-}
